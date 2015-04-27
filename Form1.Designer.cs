024d0e9441dc32aa03289d1b23afbc05b666ad98
BonsDevelopment <ninjajesus@ymail.com>
2015-04-27 14:02:03 -0700

    Source for Focus Window
    Used in case you click off screen from a game. Refocuses the window.

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace WIndow_Switcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbl_title = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.StartBTN = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.proc_text = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.lbl_title);
+            this.panel1.Controls.Add(this.listView1);
+            this.panel1.Controls.Add(this.StartBTN);
+            this.panel1.Controls.Add(this.proc_text);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(215, 232);
+            this.panel1.TabIndex = 0;
+            // 
+            // lbl_title
+            // 
+            this.lbl_title.AutoSize = true;
+            this.lbl_title.Location = new System.Drawing.Point(3, 215);
+            this.lbl_title.Name = "lbl_title";
+            this.lbl_title.Size = new System.Drawing.Size(0, 13);
+            this.lbl_title.TabIndex = 4;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Location = new System.Drawing.Point(6, 3);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(202, 171);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Process Name";
+            this.columnHeader1.Width = 90;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "PID";
+            this.columnHeader2.Width = 40;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Memory";
+            // 
+            // StartBTN
+            // 
+            this.StartBTN.Location = new System.Drawing.Point(144, 204);
+            this.StartBTN.Name = "StartBTN";
+            this.StartBTN.Size = new System.Drawing.Size(55, 23);
+            this.StartBTN.TabIndex = 2;
+            this.StartBTN.Text = "Start";
+            this.StartBTN.UseVisualStyleBackColor = true;
+            this.StartBTN.Click += new System.EventHandler(this.StartBTN_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 183);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Process Name: ";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // proc_text
+            // 
+            this.proc_text.Location = new System.Drawing.Point(85, 180);
+            this.proc_text.Name = "proc_text";
+            this.proc_text.ReadOnly = true;
+            this.proc_text.Size = new System.Drawing.Size(114, 20);
+            this.proc_text.TabIndex = 1;
+            this.proc_text.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.proc_text_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 204);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(24, 22);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "⟲";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(215, 232);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Focus Window";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button StartBTN;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label lbl_title;
+        private System.Windows.Forms.TextBox proc_text;
+        private System.Windows.Forms.Button button1;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WIndow_Switcher
+{
+    public partial class Form1 : Form
+    {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern void SwitchToThisWindow(IntPtr hWnd, bool fAltTab);
+
+        public Form1()
+        {
+            InitializeComponent();
+            MaximizeBox = false;
+            listView1.GridLines = true;
+            listView1.View = View.Details;
+            process_List();
+            
+            
+            
+        }
+        
+
+        void process_List()
+        {
+            Process[] processlist = Process.GetProcesses();
+            foreach (Process theprocess in processlist)
+            {
+                var memory = (theprocess.WorkingSet64 / 1024) / 1024;
+                string check = theprocess.ProcessName;
+
+
+
+
+
+                ListViewItem item = new ListViewItem(new[] {check,theprocess.Id.ToString(),
+                                                                    memory.ToString() +" mb"
+                                                                    });
+				                                     	
+				                                     	
+				                                     
+                string winCheck = theprocess.MainWindowTitle;
+                if (!String.IsNullOrEmpty(winCheck))
+                {
+                    listView1.Items.Add(item);
+                }
+                else
+                {
+                    //do nothing
+                }
+                
+
+            }
+            
+           
+
+        }
+
+        void FadeAway(double Speed, int WaitSpeed )
+        {
+            for (double x = 1; x > 0; x = x - Speed)
+            {
+                this.Opacity = x;
+                System.Threading.Thread.Sleep(WaitSpeed);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Process[] pname = Process.GetProcessesByName(proc_text.Text);
+            if (pname.Length == 0)
+            {
+                this.Close();
+            }
+            ShowInTaskbar = false;
+
+            foreach (Process p in Process.GetProcessesByName(proc_text.Text))
+            {
+                SwitchToThisWindow(p.MainWindowHandle, true);
+  
+            }
+        }
+
+        private void StartBTN_Click(object sender, EventArgs e)
+        {
+
+            Process[] pname = Process.GetProcessesByName(proc_text.Text);
+            if (pname.Length == 0)
+            {
+                MessageBox.Show(this,"Not a valid process","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+                
+            else{
+
+            timer1.Start();
+            FadeAway(0.10,30);
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+
+            if (listView1.SelectedIndices.Count == 0)
+            {
+                //nothing
+            }
+            else
+            {
+                proc_text.Text = listView1.SelectedItems[0].Text;
+                
+            }
+
+        }
+
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            process_List();
+        }
+
+        private void proc_text_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                e.Handled = true;
+
+                if (!proc_text.AcceptsReturn)
+                {
+
+                    StartBTN.PerformClick();
+                }
+            }
+        }
+
+        
+
+       
+
+        
+
+        
+    }
+}
